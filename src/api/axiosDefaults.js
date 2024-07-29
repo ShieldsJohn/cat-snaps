f14d664d205a8ea21f4822b3fe8fda16d4d39418
John Shields <shields101@hotmail.com>
2024-07-29 17:07:40 +0000

    Connect to API and add Sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-api-cs-8ddd0772a0f2.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
