0f9f6f17859e7df319fc11ab9f218e6b958ad153
John Shields <shields101@hotmail.com>
2024-08-04 17:40:47 +0000

    Add logged in icons to navbar and sign out functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,65 @@ import React from 'react';
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from '../styles/Navbar.module.css';
 import { NavLink } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
+import axios from 'axios';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>
+  const setCurrentUser= useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post('dj-rest-auth/logout/');
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPostIcon= (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create" >
+      <i className='fas fa-plus-square'></i> Add post</NavLink>
+  )
+
+  const loggedInIcons = <>
+  <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/feed" >
+      <i className='fas fa-stream'></i> Feed</NavLink>
+
+      <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/saved" >
+      <i className='fa-solid fa-bookmark'></i> Saved</NavLink>
+
+      <NavLink
+      className={styles.NavLink}
+      to="/"
+      onClick={handleSignOut}
+      >
+      <i className='fas fa-sign-out-alt'></i> Sign out</NavLink>
+
+      <NavLink
+      className={styles.NavLink}
+      to={`/profiles/${currentUser?.profile_id}`}
+      >
+      <Avatar src={currentUser?.profile_image} text="Profile" height={40} /></NavLink>
+  
+  </>
   const loggedOutIcons = <>
     <NavLink
       className={styles.NavLink}
       activeClassName={styles.Active}
       to="/signup" >
       <i className='fas fa-user-plus'></i> Sign up</NavLink>
+
     <NavLink
       className={styles.NavLink}
       activeClassName={styles.Active}
@@ -27,6 +75,8 @@ const NavBar = () => {
             <i class="fa-sharp fa-solid fa-cat fa-lg"></i></h5>
           </Navbar.Brand>
         </NavLink>
+
+        {currentUser && addPostIcon}
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
