8b67b3b20c5244967ad2101316f2fe170a349633
John Shields <shields101@hotmail.com>
2024-08-05 18:59:47 +0000

    Create burger menu toggle functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,13 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser= useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -68,7 +71,7 @@ const NavBar = () => {
       <i className='fas fa-sign-in'></i> Sign in</NavLink></>
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed='top'>
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed='top'>
       <Container>
         <NavLink to="/">
           <Navbar.Brand href="home"><h5>Cat Snaps
@@ -78,7 +81,7 @@ const NavBar = () => {
 
         {currentUser && addPostIcon}
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
