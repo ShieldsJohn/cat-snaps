7123e4840d9090a469211b38a9f2eb7138217b26
John Shields <shields101@hotmail.com>
2024-08-26 16:27:16 +0000

    Add refetch posts on user login/out & add Heroku deploy cmds

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,11 +16,14 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 import NewProfiles from "../profiles/NewProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const currentUser = useCurrentUser();
 
   const [query, setQuery] = useState("");
 
@@ -43,7 +46,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
